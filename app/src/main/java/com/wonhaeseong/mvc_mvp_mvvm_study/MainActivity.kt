d1c182628003e27cc89e853dbf651aeb96dced8f
want8607 <gotjd8607@gmail.com>
2024-01-02 22:21:07 +0900

    init

---
+++ app/src/main/java/com/wonhaeseong/mvc_mvp_mvvm_study/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.wonhaeseong.mvc_mvp_mvvm_study
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
