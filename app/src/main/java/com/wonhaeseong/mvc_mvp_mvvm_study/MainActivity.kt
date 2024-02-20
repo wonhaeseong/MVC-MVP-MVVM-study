f9afb924b3315f7188c84b15b0d9137d8e2e58b8
want8607 <84075111+want8607@users.noreply.github.com>
2024-02-20 17:35:01 +0900

    MainAcitivity에 ItemListView 기능 추가

--- app/src/main/java/com/wonhaeseong/mvc_mvp_mvvm_study/MainActivity.kt
+++ app/src/main/java/com/wonhaeseong/mvc_mvp_mvvm_study/MainActivity.kt
@@ -2,22 +2,25 @@ package com.wonhaeseong.mvc_mvp_mvvm_study
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ListView
 import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity(), View {
+    private lateinit var basket: Basket
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        basket = Basket()
         val basketView = layoutInflater.inflate(R.layout.basket, null)
-        val basket = BottomSheetDialog(this)
-        basket.setContentView(basketView)
-        basket.behavior.skipCollapsed = true
-
+        val bottomSheetDialog = BottomSheetDialog(this)
+        bottomSheetDialog.setContentView(basketView)
+        bottomSheetDialog.behavior.skipCollapsed = true
+        val itemListView = findViewById<ListView>(R.id.item_list)
+        itemListView.adapter = MainListAdapter(this, listOf()) // TODO: Shop(model)에서 List<Item> 가져오기
         val basketBtn = findViewById<FloatingActionButton>(R.id.basket_btn)
         basketBtn.setOnClickListener {
-            basket.show()
+            bottomSheetDialog.show()
         }
     }
 
