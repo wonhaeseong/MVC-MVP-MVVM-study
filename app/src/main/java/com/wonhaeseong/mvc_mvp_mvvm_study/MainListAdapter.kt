934751f65e6bc746851fec89e91f0e25d2a7a138
want8607 <84075111+want8607@users.noreply.github.com>
2024-02-24 14:39:37 +0900

    MainListAdapter item view 설정 완료

--- app/src/main/java/com/wonhaeseong/mvc_mvp_mvvm_study/MainListAdapter.kt
+++ app/src/main/java/com/wonhaeseong/mvc_mvp_mvvm_study/MainListAdapter.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.BaseAdapter
+import android.widget.ImageView
+import android.widget.TextView
 
 class MainListAdapter(
     context: Context,
@@ -20,7 +22,11 @@ class MainListAdapter(
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
         val viewHolder: View =
             convertView ?: inflater.inflate(R.layout.main_list_item, parent, false)
-        TODO("Item view 설정")
+        viewHolder.findViewById<ImageView>(R.id.main_item_img).apply {
+            setImageResource(items[position].imgSrc)
+            scaleType = ImageView.ScaleType.CENTER_CROP
+        }
+        viewHolder.findViewById<TextView>(R.id.main_item_name).text = items[position].name
         return viewHolder
     }
 }
