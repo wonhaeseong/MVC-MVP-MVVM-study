80dfe94cbb1fb74e65d73057be312d415c877e4c
want8607 <84075111+want8607@users.noreply.github.com>
2024-03-04 17:12:56 +0900

    ViewModel, Livedata 의존성 추가

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,4 +45,8 @@ dependencies {
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     testImplementation("org.mockito.kotlin:mockito-kotlin:5.2.1")
+
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.7.0")
+    testImplementation("androidx.arch.core:core-testing:2.2.0")
 }
---
+++ app/src/main/java/com/wonhaeseong/mvc_mvp_mvvm_study/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.wonhaeseong.mvc_mvp_mvvm_study
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel: ViewModel() {
+
+}
